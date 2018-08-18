8a23b74c65198079bdd7adb7c7e33625139d0f1d
nihakue <walden.gabrielw@gmail.com>
2018-08-19 00:36:17 +0100

    SPELLING

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>This is a placeholder for my site.</p>
     <p>
-      In the meantime, checkout my{' '}
+      In the meantime, check out my{' '}
       <a href="https://www.linkedin.com/in/gabriel-west-72866340/">LinkedIn</a>
     </p>
   </Layout>
